b8078d7e05fe6dde6c27f65071eeb4b02880ec30
Brandon Fannin <bfannin13@gmail.com>
2020-04-30 22:21:41 -0400

    updated interface for mobile first. added forward and backward controls. Changed screenshot

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,13 +1,13 @@
 <template lang="pug">
   b-container#app
     div
-      b-navbar(toggleable="sm" type="light" variant="light")
+      b-navbar(toggleable="sm" type="dark" variant="dark")
         b-navbar-toggle(target="nav-text-collapse")
         b-navbar-brand booklet
         b-collapse(id="nav-text-collapse" is-nav)
           b-navbar-nav
-            b-nav-text {{ title }} 
-              span.author-text by {{ author }}
+            b-nav-item Home 
+            b-nav-item About
     Booklet
 </template>
 
@@ -18,14 +18,6 @@ export default {
   name: "App",
   components: {
     Booklet
-  },
-  computed: {
-    title() {
-      return this.$store.state.booklet.title;
-    },
-    author() {
-      return this.$store.state.booklet.author;
-    }
   }
 };
 </script>
@@ -39,9 +31,5 @@ export default {
   color: #2c3e50;
   padding: 0;
   margin: 0;
-}
-
-.author-text {
-  font-size: .8rem;
 }
 </style>
--- frontend/src/components/Booklet.vue
+++ frontend/src/components/Booklet.vue
@@ -1,16 +1,24 @@
 <template lang="pug">
   b-container#flip
+    b-row#booklet-info
+      div {{ title }}
+      div {{ author }} 
+      div pos(act: {{ curr.act + 1}} | scene: {{ curr.scene + 1 }} | paragraph: {{ curr.para + 1 }} | sentence: {{ curr.sent + 1}})
     b-row
       div#stage
     b-row#controls
-      b-col(sm="2")
-        label(for="speed-range") speed
-      b-col(sm="8")
+      b-col(xs="2")
+        b-button(variant="dark" @click="curr = prev; changed = true")
+          b-icon-chevron-double-left
+      b-col(xs="6")
         b-form-input.speed-range(type="range" v-model="timing" min="0" max="2" step="0.1")
-      b-col(sm="2")
+      b-col(xs="2")
         b-button(:variant="going ? 'danger' : 'success'" @click="controlFlow") 
           b-icon-pause-fill(v-if="going")
           b-icon-play(v-if="!going")
+      b-col(xs="2")
+        b-button(variant="dark" @click="curr = next; changed = true")
+          b-icon-chevron-double-right
 </template>
 
 <script>
@@ -22,7 +30,8 @@ export default {
       wait: 250,
       word: "begin",
       page: "begin the first page",
-      curr: { act: 0, scene: 0, para: 0, sent: 0, word: 0 }
+      curr: { act: 0, scene: 0, para: 0, sent: 0, word: 0 },
+      changed: false
     };
   },
   methods: {
@@ -40,12 +49,10 @@ export default {
         return;
       }
 
-      if (this.curr.para == 0) {
-        console.log('showing page pause')
+      if (this.curr.para == 0 && this.curr.sent == 0 && this.curr.word == 0) {
         this.showPagePause().then(() => this.runThroughSentence());
       } else {
         this.runThroughSentence();
-        console.log('running through sentence')
       }
     },
     runThroughSentence() {
@@ -63,8 +70,16 @@ export default {
               words,
               word
             );
-            if (word + 1 < words.length) {
-              focusWord(word + 1);
+            if (!v.going) {
+              return;
+            }else if (word + 1 < words.length) {
+              if (!v.changed){
+                focusWord(word + 1);
+              }else {
+                v.changed = false;
+                v.curr.word = 0;
+                v.updateCanvas();
+              }
             } else {
               //set up next page
               v.curr = v.next;
@@ -72,11 +87,12 @@ export default {
             }
           });
         };
+
         focusWord(v.curr.word);
       }
     },
     showPagePause() {
-      document.getElementById("stage").innerHTML = " ";
+      document.getElementById("stage").innerHTML = '<b-icon-circle-half id="pause-it" />';
       return new Promise(resolve => setTimeout(resolve, this.pause * 4));
     },
     createPageElement(words, i) {
@@ -125,6 +141,43 @@ export default {
       ];
       return sentence;
     },
+    prev() {
+      if (this.curr.sent - 1 >= 0) {
+        return {
+          act: this.curr.act,
+          scene: this.curr.scene,
+          para: this.curr.para,
+          sent: this.curr.sent - 1,
+          word: 0
+        }
+      } else if (this.curr.para - 1 >= 0) {
+        return {
+          act: this.curr.act,
+          scene: this.curr.scene,
+          para: this.curr.para - 1,
+          sent: 0,
+          word: 0
+        }
+      } else if (this.curr.scene - 1 >= 0) {
+        return {
+          act: this.curr.act,
+          scene: this.curr.scene - 1,
+          para: 0,
+          sent: 0,
+          word: 0
+        }
+      } else if (this.curr.act - 1 >= 0) {
+        return {
+          act: this.curr.act - 1,
+          scene: 0,
+          para: 0,
+          sent: 0,
+          word: 0
+        }
+      }else {
+        return { act: 0, scene: 0, para: 0, sent: 0, word: 0 };
+      }
+    },
     next() {
       let totalActs = this.acts.length - 1;
       let thisAct = this.acts[this.curr.act];
@@ -136,7 +189,6 @@ export default {
       let thisPara = thisScene[this.curr.para];
 
       let sentsInPara = thisPara.length - 1;
-      //let thisSentence = thisPara[this.curr.sent];
 
       if (this.curr.sent + 1 <= sentsInPara) {
         return {
@@ -186,10 +238,25 @@ export default {
 <style lang="sass">
 #stage
   width: 100%
-  height: 80%
+  font-size: 7.5vw
   .highlighted-word
     background-color: yellow
+  margin: 5%
 #flip
   padding: 0
   margin: 0
+#booklet-info
+  display: flex
+  flex-flow: column
+  justify-content: center
+  margin: 2%
+  border-bottom: 2px solid black
+#pause-it
+  margin: 40%
+  font-size: 3rem
+#controls
+  background-color: #343a40
+  padding: 2%
+  position: fixed
+  bottom: 5%
 </style>
