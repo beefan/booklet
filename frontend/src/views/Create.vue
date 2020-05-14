b6bda3e5a623321b599c9b37ae299f53b85a814b
Brandon Fannin <bfannin13@gmail.com>
2020-05-14 16:02:12 -0400

    support for paste in data; parses for formatting commands

--- frontend/src/views/Create.vue
+++ frontend/src/views/Create.vue
@@ -1,17 +1,17 @@
 <template lang="pug">
   div#syntax-playground
+    b-row#create-footer
+      b-col.clickable(xs="6") 
+        b-button(:variant="showFormatHelp ? 'danger' : 'info'" @click="showFormatHelp = !showFormatHelp; showPasteBox = false") {{`${ showFormatHelp ? 'hide ' : 'show '} format help`}}
+      b-col.clickable(xs="6") 
+        b-button(:variant="showPasteBox ? 'danger' : 'info'" @click="processPaste()") {{`${ showPasteBox ? 'process ' : ''} paste`}}
     b-row#create-header
       b-col(xs="8") {{ booklet.title }}
       b-col(xs="4") {{ booklet.author }}
-    b-row#create-footer
-      b-col.clickable(xs="6") 
-        b-button(:variant="showFormatHelp ? 'danger' : 'info'" @click="showFormatHelp = !showFormatHelp") {{`${ showFormatHelp ? 'hide ' : 'show '} format help`}}
-      b-col.clickable(xs="6") 
-        b-button(:variant="showPasteBox ? 'danger' : 'info'" @click="processPaste()") {{`${ showPasteBox ? 'process ' : ''} paste`}}
     b-row(v-if="showFormatHelp") 
       b-table(striped hover :items="formattingHelp" head-variant="dark" small bordered)
     b-row(v-if="showPasteBox")
-      b-form-textarea
+      b-form-textarea#paste-box
     b-form-group(v-for="(scene, index) in booklet.scenes" 
     :description="`highlight-color: ${scene.format.hltColor ? scene.format.hltColor : 'default'} | speed: ${scene.format.speed ? scene.format.speed : 'default'}`"
     )
@@ -21,7 +21,7 @@
       :value="scene.text" 
       size="sm" rows="5" 
       :formatter="formatScene"
-      :style="`${scene.format.color ? 'color: ' + scene.format.color : ''}${scene.format.bgColor ? ' background: ' + scene.format.bgColor : ''}`"
+      :style="{color: scene.format.color, backgroundColor: scene.format.bgColor}"
       )
 
 </template>
@@ -61,14 +61,64 @@ export default {
       this.booklet.scenes.push({});
     },
     processPaste() {
+      this.showFormatHelp = false;
+      const paste = document.getElementById('paste-box');
+      if (this.showPasteBox && paste.value) {
+        this.setBookletFromPaste(paste.value)
+        paste.value = ''
+      }
       this.showPasteBox = !this.showPasteBox
+    },
+    setBookletFromPaste(raw) {
+      let booklet = {title: '', author: '', scenes: []}
+      let scenes = raw.split('!/n/')
+      scenes.forEach( scene => {
+        booklet.scenes.push({text: scene, title:'', format:{}});
+      })
+      this.booklet = booklet
+
+      this.booklet.scenes = this.booklet.scenes.map( (scene, index) => {
+        return this.runPasteCommands(scene, index)
+      });
+
+      this.booklet.scenes.forEach( (x, i) => {
+        if (x.text === '') {
+          this.booklet.scenes.splice(i, 1)
+        }
+      })
+
+    },
+    runPasteCommands(scene, index) {
+      let lines = scene.text.split('\n')
+      lines = lines.map(line => {
+        if (this.isCommand(line)) {
+          if(this.processCmd(this.getCommand(line), `scene:${index}`)){
+            return ''
+          } else {/*a command failed; do nothing for now */}
+        }
+        return line;
+      })
+      
+      lines.forEach((line, index) => {
+        if (line === '') {
+          lines.splice(index, 1)
+        }
+      })
+
+      scene.text = lines.join('\n').trim()
+      return scene
+    },
+    isCommand(str) {
+      return (str.substring(0,2) === '!/' && str.substring(str.length - 1) === '/')
+    },
+    getCommand(str) {
+      return str.substring(2, str.length - 1)
     },
     formatScene(value, e) {
       let n = value.split('\n')
-
       n = n.map(line => {
-        if (line.substring(0,2) === '!/' && line.substring(line.length - 1) === '/') {
-          if (this.processCmd(line.substring(2, line.length - 1), e.target)){
+        if (this.isCommand(line)) {
+          if (this.processCmd(this.getCommand(line), e.target)){
             return '' 
           }
         }
@@ -78,6 +128,7 @@ export default {
       return n.join('\n');
     },
     processCmd(cmd, elem) {
+      if (!elem) { return false }
       let eqI = cmd.indexOf('=')
       let command = cmd
       let payload = ''
@@ -85,7 +136,7 @@ export default {
         command = cmd.substring(0, eqI)
         payload = cmd.substring(eqI + 1)
       }
-      let sceneI = elem.id.split(':')[1]
+      let sceneI = `${elem.id ? elem.id : elem}`.split(':')[1]
       let thisScene = this.booklet.scenes[sceneI]
       switch(command) {
         case ' ':
@@ -142,7 +193,12 @@ export default {
 
       return true;
     },
-
+  computed: {
+    sceneStyle() {
+      return 'color: white';
+      //return 
+    },
+  }
   }
 }
 </script>
